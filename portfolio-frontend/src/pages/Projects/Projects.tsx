b1bb3007caed8a101cc33e29dd627f4cdf2ef8f0
moosewithwolf <moosewithwolf@gmail.com>
2025-05-20 00:41:52 -0400

    deploy final test

--- portfolio-frontend/src/pages/Projects/Projects.tsx
+++ portfolio-frontend/src/pages/Projects/Projects.tsx
@@ -3,6 +3,7 @@ function Projects() {
       <section className="space-y-4">
         <h2 className="text-2xl font-semibold">Projects</h2>
         <p className="text-gray-700">Here are some of the things I've worked on.</p>
+        <p className="text-gray-700">deploy test</p>
       </section>
     )
   }
