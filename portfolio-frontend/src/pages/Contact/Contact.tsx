6e3ede4443ca189243fdc2bb6eb832faa6936ba9
moosewithwolf <moosewithwolf@gmail.com>
2025-09-06 16:51:30 -0400

    deploy test after server expansion

--- portfolio-frontend/src/pages/Contact/Contact.tsx
+++ portfolio-frontend/src/pages/Contact/Contact.tsx
@@ -3,6 +3,7 @@ function Contact() {
       <section className="space-y-4">
         <h2 className="text-2xl font-semibold">Contact</h2>
         <p className="text-gray-700">email, github...</p>
+        <p className="text-gray-700">test after server expansion</p>
       </section>
     )
   }
